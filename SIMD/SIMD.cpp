ad2c5ba578fdadd5a0c83ebb7c1b1f85a30a3ce9
MaryPhone <bleezemlf@163.com>
2021-03-24 16:22:31 +0800

    complete matrix multiply

--- SIMD/SIMD.cpp
+++ SIMD/SIMD.cpp
@@ -9,75 +9,75 @@ using namespace std;
 class MatMult
 {
 protected:
-	float ** m_m1;
-	float ** m_m2;
-	int  m_size;
-	float ** m_r;
+	float ** _m1;
+	float ** _m2;
+	int  _size;
+	float ** _r;
 public:
 	MatMult(int size , float value)
 	{
-		m_size = size;
-		m_m1 = new float* [m_size];
-		for (int i = 0; i < m_size; i++)
+		_size = size;
+		_m1 = new float* [_size];
+		for (int i = 0; i < _size; i++)
 		{
-			m_m1[i] = new float[m_size];
+			_m1[i] = new float[_size];
 		}
-		for (int i = 0; i < m_size; i++)
+		for (int i = 0; i < _size; i++)
 		{
-			for (int j = 0; j < m_size; j++)
+			for (int j = 0; j < _size; j++)
 			{
-				m_m1[i][j] = value;
+				_m1[i][j] = value;
 			}
 		}
-		m_m2 = new float* [m_size];
-		for (int i = 0; i < m_size; i++)
+		_m2 = new float* [_size];
+		for (int i = 0; i < _size; i++)
 		{
-			m_m2[i] = new float[m_size];
+			_m2[i] = new float[_size];
 		}
-		for (int i = 0; i < m_size; i++)
+		for (int i = 0; i < _size; i++)
 		{
-			for (int j = 0; j < m_size; j++)
+			for (int j = 0; j < _size; j++)
 			{
-				m_m2[i][j] = value;
+				_m2[i][j] = value;
 			}
 		}
-		m_r = new float* [m_size];
-		for (int i = 0; i < m_size; i++)
+		_r = new float* [_size];
+		for (int i = 0; i < _size; i++)
 		{
-			m_r[i] = new float[m_size];
+			_r[i] = new float[_size];
 		}
-		for (int i = 0; i < m_size; i++)
+		for (int i = 0; i < _size; i++)
 		{
-			for (int j = 0; j < m_size; j++)
-				m_r[i][j] = 0;
+			for (int j = 0; j < _size; j++)
+				_r[i][j] = 0;
 		}
 	}
 
-	~MatMult()
+	virtual ~MatMult()
 	{
-		for (int i = 0; i < m_size; i++) {
-			delete[] m_m1[i];
+		for (int i = 0; i < _size; i++) {
+			delete[] _m1[i];
 		}
-		delete[] m_m1;
-		for (int i = 0; i < m_size; i++) {
-			delete[] m_m2[i];
+		delete[] _m1;
+		for (int i = 0; i < _size; i++) {
+			delete[] _m2[i];
 		}
-		delete[] m_m2;
-		for (int i = 0; i < m_size; i++) {
-			delete[] m_r[i];
+		delete[] _m2;
+		for (int i = 0; i < _size; i++) {
+			delete[] _r[i];
 		}
-		delete[] m_r;
+		delete[] _r;
 	}
 
 	virtual void matMult() = 0;
 	
 	void matShowR()
 	{
-		for (int i = 0; i < m_size; i++)
+		for (int i = 0; i < _size; i++)
 		{
-			for (int j = 0; j < m_size; j++)
+			for (int j = 0; j < _size; j++)
 			{
-				cout << m_r[i][j];
+				cout << _r[i][j];
 			}
 			cout << endl;
 		}
@@ -85,24 +85,24 @@ public:
 
 	void matTransM2()
 	{
-		int t;
-		for (int i = 0; i < m_size; i++)
+		float t;
+		for (int i = 0; i < _size; i++)
 		{
 			for (int j = 0; j < i; j++)
 			{
-				t = m_m2[i][j];
-				m_m2[i][j] = m_m2[j][i];
-				m_m2[j][i] = t;
+				t = _m2[i][j];
+				_m2[i][j] = _m2[j][i];
+				_m2[j][i] = t;
 			}
 		}
 	}
 
 	void resetR()
 	{
-		for (int i = 0; i < m_size; i++)
+		for (int i = 0; i < _size; i++)
 		{
-			for (int j = 0; j < m_size; j++)
-				m_r[i][j] = 0;
+			for (int j = 0; j < _size; j++)
+				_r[i][j] = 0;
 		}
 	}
 };
@@ -114,20 +114,49 @@ public:
 	void matMult()
 	{
 		auto startT = std::chrono::steady_clock::now();
-		for (int i = 0; i < m_size; i++)
+		for (int i = 0; i < _size; i++)
 		{
-			for (int j = 0; j < m_size; j++)
+			for (int j = 0; j < _size; j++)
 			{
-				for (int n = 0; n < m_size; n++)
+				for (int n = 0; n < _size; n++)
 				{
-					m_r[i][j] += m_m1[i][n] * m_m2[n][j];
+					_r[i][j] += _m1[i][n] * _m2[n][j];
 				}
 			}
 		}
 		auto endT = std::chrono::steady_clock::now();
 		std::chrono::duration<double, std::micro> elapsed = endT - startT;
-		cout << "总占用时间:" << right << setw(10) << fixed << setprecision(3) 
-				<< (double)elapsed.count() / 1000 << "ms" << endl;
+		cout << right << setw(5)
+			<< "1" << '\t' << _size
+			<< setw(11) << fixed << setprecision(3)
+			<< (double)elapsed.count() / 1000 << "ms" << endl;
+	}
+};
+
+class MatMultAlg :public MatMult
+{
+public:
+	using MatMult::MatMult;
+	void matMult()
+	{
+		matTransM2();
+		auto startT = std::chrono::steady_clock::now();
+		for (int i = 0; i < _size; ++i)
+		{
+			for (int j = 0; j < _size; ++j)
+			{
+				float temp = 0;
+				for (int k = 0; k < _size; ++k)
+					temp += _m1[i][k] * _m2[j][k];
+				_r[i][j] = temp;
+			}
+		}
+		auto endT = std::chrono::steady_clock::now();
+		std::chrono::duration<double, std::micro> elapsed = endT - startT;
+		cout << right << setw(5)
+			<< '1' << '\t' << _size
+			<< setw(11) << fixed << setprecision(3)
+			<< (double)elapsed.count() / 1000 << "ms" << endl;
 	}
 };
 
@@ -142,16 +171,15 @@ public:
 	{
 		for (int i = start; i < start + cnt; ++i)
 		{
-			for (int j = 0; j < m_size; ++j)
+			for (int j = 0; j < _size; j++)
 			{
-				int temp = 0;
-				for (int k = 0; k < m_size; ++k)
-					temp += m_m1[i][k] * m_m2[j][k];
-				m_r[i][j] = temp;
+				for (int n = 0; n < _size; n++)
+				{
+					_r[i][j] += _m1[i][n] * _m2[n][j];
+				}
 			}
 		}
 	}
-	
 
 	void matMultTh()
 	{
@@ -160,14 +188,14 @@ public:
 		vector<thread> th;
 		for (int i = 1; i <= 8; i++)
 		{
-			int step = m_size / i;
+			int step = _size / i;
 			resetR();
 			vector<thread> th;
 			auto startT = std::chrono::steady_clock::now();
 			for (int j = 0; j < i; j++)
 			{
 				if (j == i - 1)
-					th.push_back(thread(f, j * step, m_size - j * step));
+					th.push_back(thread(f, j * step, _size - j * step));
 				else
 					th.push_back(thread(f, j * step, step));
 			}
@@ -175,18 +203,194 @@ public:
 				i.join();
 			auto endT = std::chrono::steady_clock::now();
 			std::chrono::duration<double, std::micro> elapsed = endT - startT;
-			cout << "总占用时间:" << right << setw(10) << fixed 
-				<< setprecision(3)<< (double)elapsed.count() / 1000 << "ms" << endl;
+			cout << right << setw(5)
+				<< i << '\t' << _size
+				<< setw(11) << fixed << setprecision(3)
+				<< (double)elapsed.count() / 1000 << "ms" << endl;
 		}
 	}
 };
 
+class MatMultSIMD :public MatMult
+{
+protected:
+	__m256** _mm1;
+	__m256** _mm2;
+public:
+	MatMultSIMD(int size,float value) :MatMult(size, value)
+	{
+		size = _size / 8;
+
+		_mm1 = new __m256*[_size];
+		for (int i = 0; i < _size; i++)
+		{
+			_mm1[i] = new __m256[size];
+		}
+		size = _size / 8;
+		for (int i = 0; i < _size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				_mm1[i][j] = _mm256_loadu_ps(&_m1[i][j * 8]);
+			}
+		}
+
+		_mm2 = new __m256* [_size];
+		for (int i = 0; i < _size; i++)
+		{
+			_mm2[i] = new __m256[size];
+		}
+		matTransM2();
+		for (int i = 0; i < _size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				_mm2[i][j] = _mm256_loadu_ps(&_m2[i][j * 8]);
+			}
+		}
+	}
+
+	~MatMultSIMD()
+	{
+		for (int i = 0; i < _size; i++) {
+			delete[] _mm1[i];
+		}
+		delete[] _mm1;
+		for (int i = 0; i < _size; i++) {
+			delete[] _mm2[i];
+		}
+		delete[] _mm2;
+	}
+
+	void matMult()
+	{
+		int size = _size / 8;
+		auto startT = std::chrono::steady_clock::now();
+		for (int i = 0; i < _size; i++)
+		{
+			for (int j = 0; j < _size; j++)
+			{
+				__m256 t = _mm256_set1_ps(0);
+				for (int k = 0; k < size; k++)
+				{
+					t = _mm256_add_ps(t, _mm256_mul_ps(_mm1[i][k], _mm2[j][k]));
+				}
+				float sum = 0;
+				float* ptr = (float*)(&t);
+				for (int pos = 0; pos < 8; pos++)
+					sum += ptr[pos];
+				_r[i][j] = sum;
+			}
+		}
+		auto endT = std::chrono::steady_clock::now();
+		std::chrono::duration<double, std::micro> elapsed = endT - startT;
+		cout << right << setw(5)
+			<< '1' << '\t' << _size
+			<< setw(11) << fixed << setprecision(3)
+			<< (double)elapsed.count() / 1000 << "ms" << endl;
+	}
+};
+
+class MatMultBest :public MatMultSIMD
+{
+public:
+	using MatMultSIMD::MatMultSIMD;
+	void matMult(int start, int cnt)
+	{
+		int size = _size / 8;
+		for (int i = start; i < start + cnt; i++)
+		{
+			for (int j = 0; j < _size; j++)
+			{
+				__m256 t = _mm256_set1_ps(0);
+				for (int k = 0; k < size; k++)
+				{
+					t = _mm256_add_ps(t, _mm256_mul_ps(_mm1[i][k], _mm2[j][k]));
+				}
+				float sum = 0;
+				float* ptr = (float*)(&t);
+				for (int pos = 0; pos < 8; pos++)
+					sum += ptr[pos];
+				_r[i][j] = sum;
+			}
+		}
+	}
+	void matMultTh()
+	{
+		auto f = [&](int start, int cnt) {matMult(start, cnt); };
+		vector<thread> th;
+		for (int i = 1; i <= 8; i++)
+		{
+			int step = _size / i;
+			resetR();
+			vector<thread> th;
+			auto startT = std::chrono::steady_clock::now();
+			for (int j = 0; j < i; j++)
+			{
+				if (j == i - 1)
+					th.push_back(thread(f, j * step, _size - j * step));
+				else
+					th.push_back(thread(f, j * step, step));
+			}
+			for (auto& i : th)
+				i.join();
+			auto endT = std::chrono::steady_clock::now();
+			std::chrono::duration<double, std::micro> elapsed = endT - startT;
+			cout << right << setw(5)
+				<< i << '\t' << _size
+				<< setw(11) << fixed << setprecision(3)
+				<< (double)elapsed.count() / 1000 << "ms" << endl;
+		}
+	}
+};
+
+
+
 int main()
 {
-	//MatMultCommon s1(512, 1.0);
-	//s1.matMult();
-	
-	MatMultThreads s2(1024, 1.0);
-	s2.matMultTh();
-	
+	/*cout << "common algorithm:" << endl << endl;
+	for (int i = 256; i <= 2048; i += 256)
+	{
+		MatMultCommon* s = new MatMultCommon(i, 1.0);
+		s->matMult();
+		delete s;
+	}
+	cout << endl;
+
+	cout << "best algorithm:" << endl << endl;
+	for (int i = 256; i <= 2048; i += 256)
+	{
+		MatMultAlg* s = new MatMultAlg(i, 1.0);
+		s->matMult();
+		delete s;
+	}
+	cout << endl;
+
+	cout << "multi-threads:" << endl << endl;
+	for (int i = 256; i <= 2048; i += 256)
+	{
+		MatMultThreads* s = new MatMultThreads(i, 1.0);
+		s->matMultTh();
+		delete s;
+	}
+	cout << endl;
+
+	cout << "only SIMD:" << endl << endl;
+	for (int i = 256; i <= 2048; i += 256)
+	{
+		MatMultSIMD* s = new MatMultSIMD(i, 1.0);
+		s->matMult();
+		delete s;
+	}
+	cout << endl;*/
+
+	cout << "best performance:" << endl << endl;
+	for (int i = 256; i <= 2048; i += 256)
+	{
+		MatMultBest* s = new MatMultBest(i, 1.0);
+		s->matMultTh();
+		s->matShowR();
+		delete s;
+	}
+	cout << endl;
 }
